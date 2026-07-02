cf8e3f97e73b4bba0125e3edf4ae32ad4a67b036
Tomandjerri <king04122005@gmail.com>
2026-07-02 05:15:59 +0000

    Sua port db

--- app/Database.php
+++ app/Database.php
@@ -3,13 +3,14 @@ class Database {
     private $host = "localhost";
     private $db_name = "quanlyxemay";
     private $username = "root";
-    private $password = "";
+    private $password = "Tnminhngoc412@";
+    private $port = 3306;
     public $conn;
 
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password, $this -> port);
             $this->conn->exec("set names utf8");
         } catch(PDOException $exception) {
             echo "Lỗi kết nối: " . $exception->getMessage();
